c39c3538f942e824a312ddac7583ffcfc851e479
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-19 21:39:45 -0800

    sorting algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,3 +1,34 @@
 #include "sort.h"
 
+/**
+ * selection_sort - a function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: the array
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, n;
+	int temp;
 
+	for (i = 0; i < size; i++)
+	{
+		n = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[n])
+			{
+				n = j;
+			}
+		}
+
+		if (n != i)
+		{
+			temp = array[n];
+			array[n] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
