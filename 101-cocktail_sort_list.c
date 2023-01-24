40bfae14118f7006be81429351ae8516b5da06e4
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-24 12:31:17 -0800

    sortin algorithg

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - a function that sorts a doubly linked list of integers
+ * in ascending order using the Cocktail shaker sort algorithm
+ *
+ * @list: doubly linked list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	d
+}
